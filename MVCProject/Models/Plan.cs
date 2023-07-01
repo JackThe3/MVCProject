425168043b58dd403d1cec6964cf167368ea081d
JackThe3 <jackulik.l@gmail.com>
2023-07-01 14:33:08 +0200

    add Plan model

---
+++ MVCProject/Models/Plan.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCProject.Models;
+
+public class Plan{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Owner { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime CreatedAt { get; set; }
+    [DataType(DataType.Date)]
+    public DateTime UpdatedAt { get; set; }
+    public bool Status { get; set; }
+    public string? UpdatedBy { get; set; }
+
+}
