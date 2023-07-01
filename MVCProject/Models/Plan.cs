bb334c7e97dd7f6005f591c067ff079864f74c6a
JackThe3 <jackulik.l@gmail.com>
2023-07-01 18:14:37 +0200

    add validation

--- MVCProject/Models/Plan.cs
+++ MVCProject/Models/Plan.cs
@@ -4,13 +4,25 @@ namespace MVCProject.Models;
 
 public class Plan{
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 1)]
+    [Required]
+    [RegularExpression(@"^[a-zA-Z][a-zA-Z0-9.,$;]+$")]
     public string? Name { get; set; }
+
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Owner { get; set; }
+    
     [DataType(DataType.DateTime)]
     public DateTime CreatedAt { get; set; }
+    
     [DataType(DataType.DateTime)]
     public DateTime UpdatedAt { get; set; }
+    
     public bool Status { get; set; }
+
+    [StringLength(60)]
     public string? UpdatedBy { get; set; }
 
 }
