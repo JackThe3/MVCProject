c9489b9af16b5e6b2f062ff2a308ee5ba121423c
JackThe3 <jackulik.l@gmail.com>
2023-07-03 23:15:22 +0200

    index page styling

--- MVCProject/wwwroot/js/site.js
+++ MVCProject/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$('#checkAll').click(function () {
+    $(':checkbox.checkItem').prop('checked', this.checked);
+});
